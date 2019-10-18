933923480b88a0bfa3db5169089e8adb5877aa16
benitogeordie <bg31@rice.edu>
2019-10-18 06:22:39 -0500

    Clang does not work for some reason

--- Testground/src/io.cpp
+++ Testground/src/io.cpp
@@ -3,19 +3,6 @@
 #include <iostream>
 #include <exception>
 
-std::string exec(const char* cmd) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        result += buffer.data();
-    }
-    return result;
-}
-
 void VectorFeatures(std::istream& in, std::vector<double>& vec, int& label, size_t& dimensions) {
 
     std::string line;
