1118f0713dae500edbff07baead18c3744684605
benitogeordie <bg31@rice.edu>
2019-10-20 17:29:51 -0500

    Moved reservoir.h from fast5 version to fasta version

--- Testground/Include/reservoir.h
+++ Testground/Include/reservoir.h
@@ -11,36 +11,37 @@
 #include <random>
 
 #include "Debug.h"
+#include "io.h"
 
 
-
-typedef std::vector< std::vector< double > > reserve;
-typedef std::map<int, int> statistics;
-typedef int remainders;
+// change reserve
+typedef std::vector< std::pair< int, std::string > > reserve;
 // Should I reserve space for vectors?
 
 class Reservoir
 {
 public:
-    Reservoir(size_t R, size_t range, size_t L, std::vector<int>& clusters, size_t total);
+    Reservoir(size_t R, size_t range, size_t L, size_t total, const char* oldDir, std::string& newDir);
     ~Reservoir();
-
-    void add(std::vector< double > vec, int *hashes);
+    
+    void add(int label, int *hashes, std::string fileName);
     void clear();
-
-    void pprint(std::ostream& out, int width, bool format, std::vector<int>& clusters);
+    
+    void pprint(std::ostream& outPath);
     void pprint(std::ostream& out, std::string& path);
     int count();
-    int countLabels(size_t& r);
+    int countLabels();
     int countElements(size_t& r);
-
-
-    private:
-        size_t _R, _range, _L;
-        reserve* _buckets;
-        statistics* _stats;
-        remainders* _rems;
-        const uint8_t magic_number = 0x4D; // magic number for binary file IO
-        const uint8_t file_version_number = 0x01; // file version number
+    
+    
+private:
+    size_t _R, _range, _L, _total;
+    reserve _sample;
+    std::set<int> _labelSet;
+    int* _race;
+    std::string _old;
+    std::string _new;
+    const uint8_t magic_number = 0x4D; // magic number for binary file IO
+    const uint8_t file_version_number = 0x01; // file version number
 };
 
